b0dc17c00cffe397d4f44cd5dcabb82df4b0480f
Zdeněk Böhm <zdenek.bohm@nic.cz>
2009-04-08 08:40:19 +0000

    move project under trunk
    git-svn-id: file:///data/svn/enum/distutils/trunk@7647 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+        name='fred-distutils',
+        version='1.0',
+        description = 'Fred Distutils',
+        author = 'Aleš Doležal, CZ.NIC',
+        author_email = 'ales.dolezal@nic.cz',
+        url = 'http://www.nic.cz/',
+        license = 'GNU GPL',
+        platforms = ['posix'],
+        long_description = 'Fred Distutils, utilities for easier way to build packages.',
+
+        packages=['freddist', 'freddist.command'],
+        package_data={'freddist': ['freddist/README']},
+
+      )
+
