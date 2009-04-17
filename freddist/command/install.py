8084c7ac55274d124ec4d0266820d77838de3ee4
Zdeněk Böhm <zdenek.bohm@nic.cz>
2009-04-17 14:34:07 +0000

    add support for preapre deb files
    git-svn-id: file:///data/svn/enum/distutils/trunk@7730 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- freddist/command/install.py
+++ freddist/command/install.py
@@ -1,8 +1,9 @@
-import os, re, sys
+import os, re, sys, stat
 from install_parent import install_parent
 from distutils.command.install import install as _install
 from distutils.debug import DEBUG
 from distutils.util import convert_path, subst_vars, change_root
+
 
 # Difference between freddist.install and distutils.install class isn't wide.
 # Only new options were added. Most of them are output directory related.
@@ -36,6 +37,51 @@ class install(_install, install_parent):
             self.record = 'install.log'
 
 
+    def make_preparation_for_debian_package(self, log, values):
+        "Prepare folder for debian package. Parameter values is in format (('FROM_PATTERN', 'to-value'), ...)"
+        
+        src_root = os.path.join('doc', 'debian')
+        dest_root = os.path.join(self.get_root(), 'DEBIAN')
+        
+        for src, dest in (
+            (os.path.join(src_root, 'postinst.install'), 
+             os.path.join(dest_root, 'postinst')), 
+            
+            (os.path.join(src_root, 'postrm.install'), 
+             os.path.join(dest_root, 'postrm')), 
+
+            (os.path.join(src_root, 'control.install'), 
+             os.path.join(dest_root, 'control')), 
+            
+            ):
+            self.replace_pattern(src, dest, values)
+            log.info('creating %s' % dest)
+        
+        # set privileges to run
+        for name in ('postinst', 'postrm'):
+            set_file_executable(os.path.join(dest_root, name))
+
+
+    def get_info_for_create_package(self, project_name, package_version):
+        body = []
+        body.append("cd %s" % self.get_root())
+        body.append('find * -type f|grep -v "^DEBIAN/"|while read x;do md5sum "${x}";done > DEBIAN/md5sums')
+        body.append("cd ..")
+        body.append("dpkg-deb -b %s %s_%s-1~hardy+1_all.deb" % (self.get_root(), project_name, package_version))
+        return '\n'.join(body)
+    
+
+
     def run(self):
         _install.run(self)
         self.normalize_record()
+
+
+
+
+def set_file_executable(filepath):
+    "Set file mode to executable"
+    os.chmod(filepath, os.stat(filepath)[stat.ST_MODE] | stat.S_IEXEC | 
+             stat.S_IXGRP | stat.S_IXOTH)
+
+
