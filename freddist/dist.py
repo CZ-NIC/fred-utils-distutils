f5e9e9c4d46b207f996c88a4ad6cba4d092a7e77
Zdeněk Böhm <zdenek.bohm@nic.cz>
2009-07-07 12:48:13 +0000

    Skip file egg-info if the debian package option is set
    git-svn-id: file:///data/svn/enum/distutils/trunk@8505 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- freddist/dist.py
+++ freddist/dist.py
@@ -45,6 +45,11 @@ except:
     from command.install_lib import install_lib
 
 try:
+    from freddist.command.install_egg_info import install_egg_info
+except:
+    from command.install_egg_info import install_egg_info
+
+try:
     from freddist.command.sdist import sdist 
 except ImportError:
     from command.sdist import sdist
@@ -114,6 +119,8 @@ class Distribution(_Distribution):
             self.cmdclass['install_scripts'] = install_scripts
         if not self.cmdclass.get('install_lib'):
             self.cmdclass['install_lib'] = install_lib
+        if not self.cmdclass.get('install_egg_info'):
+            self.cmdclass['install_egg_info'] = install_egg_info
         if not self.cmdclass.get('sdist'):
             self.cmdclass['sdist'] = sdist
         if not self.cmdclass.get('bdist'):
